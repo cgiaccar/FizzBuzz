8d52bab86d5055031ddd997da2c71aa534413906
Camilla Giaccari <cami.lla@me.com>
2021-11-17 16:16:02 +0100

    Refactor tests

--- src/test/java/dssc/assignment/fizzbuzz/Convert.java
+++ src/test/java/dssc/assignment/fizzbuzz/Convert.java
@@ -1,8 +1,8 @@
 package dssc.assignment.fizzbuzz;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -12,23 +12,14 @@ public class Convert {
     private final FizzBuzz fizzBuzz = new FizzBuzz();
 
     @ParameterizedTest
-    @CsvSource({"1, 1", "2, 2", "41, 41" })
+    @CsvSource({"1, 1", "2, 2", "41, 41"})
     void numbers_not_divisible_by_3_or_5_to_strings(int number, String expected) {
         assertEquals(expected, fizzBuzz.convert(number));
     }
 
-    @Test
-    void number_3_to_Fizz() {
-        assertEquals("Fizz", fizzBuzz.convert(3));
-    }
-
-    @Test
-    void number_6_to_Fizz() {
-        assertEquals("Fizz", fizzBuzz.convert(6));
-    }
-
-    @Test
-    void number_24_to_Fizz() {
-        assertEquals("Fizz", fizzBuzz.convert(24));
+    @ParameterizedTest
+    @ValueSource(ints = {3, 6, 24})
+    void multiples_of_three_to_Fizz(int number) {
+        assertEquals("Fizz", fizzBuzz.convert(number));
     }
 }
